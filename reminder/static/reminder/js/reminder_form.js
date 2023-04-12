47fcda869a84fb9a4864ca9d49be5c9e9e8b7e77
Brian Chiang <brian_chiang@chief.com.tw>
2023-04-12 17:07:39 +0800

    Show relevant forms on entoring reminder/change page based on policy category.

--- reminder/static/reminder/js/reminder_form.js
+++ reminder/static/reminder/js/reminder_form.js
@@ -10,7 +10,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const formRowEndAt = document.querySelector("#id_form_row_end_at");
     const formRowSpecifiedDates = document.querySelector("#id_form_row_specified_dates");
     // Initialize
-    formRowSpecifiedDates.style.display = "none";
+    if (inputPolicy.value === 'specified dates') {
+        formRowStartAt.style.display = "none";
+        formRowEndAt.style.display = "none";
+        formRowSpecifiedDates.style.display = "";
+    }
+    else if (inputPolicy.value === 'once') {
+        formRowStartAt.style.display = "";
+        formRowEndAt.style.display = "none";
+        formRowSpecifiedDates.style.display = "none";
+    }
+    else {
+        formRowStartAt.style.display = "";
+        formRowEndAt.style.display = "";
+        formRowSpecifiedDates.style.display = "none";
+    }
     // Event
     inputPolicy.addEventListener("change", function(event) {
         if (inputPolicy.value === 'specified dates') {
