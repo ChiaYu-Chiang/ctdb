95beb1e8ea26ccb0daeacf977a7e9196aa6e2034
Brian Chiang <brian_chiang@chief.com.tw>
2025-07-17 16:49:12 +0800

    add dep_calendar's detail page & delete page

--- dep_calendar/urls.py
+++ dep_calendar/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import (event_list, event_create, calendar_events_json)
+from . import views
 
 app_name = 'dep_calendar'
 
 urlpatterns = [
-    path('dep_calendar/', event_list, name='event_list'),
-    path('dep_calnedar/events/', calendar_events_json, name='calendar_events_json'),
-    path('dep_calnedar/add/', event_create, name='event_create'),
+    path('dep_calendar/', views.event_list, name='event_list'),
+    path('dep_calendar/events/json/', views.calendar_events_json, name='calendar_events_json'),
+    path('dep_calendar/event/create/', views.event_create, name='event_create'),
+    path('dep_calendar/event/<int:pk>/', views.event_detail, name='event_detail'),
+    path('dep_calendar/event/<int:pk>/change/', views.event_update, name='event_update'),
+    path('dep_calendar/event/<int:pk>/delete/', views.event_delete, name='event_delete'),
 ]
--- dep_calendar/views.py
+++ dep_calendar/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse, HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.utils.translation import gettext as _
 from .models import CalendarEvent
 from .forms import CalendarEventForm
 
@@ -34,8 +36,59 @@ def event_create(request):
             event.department = user_department
             event.save()
             form.save_m2m()
+            messages.success(request, _('事件已成功新增！'))
             return redirect('dep_calendar:event_list')
     else:
         form = CalendarEventForm(user=request.user)
     return render(request, 'dep_calendar/event_form.html', {'form': form, 'form_buttons': form_buttons})
 
+@login_required
+def event_update(request, pk):
+    event = get_object_or_404(CalendarEvent, id=pk)
+    user_department = request.user.profile.activated_role
+    
+    # 檢查權限 - 只有同部門的人才能編輯
+    if not event.can_user_access(request.user):
+        return HttpResponseForbidden("您沒有權限編輯此事件")
+    
+    form_buttons = ['update']
+    if request.method == 'POST':
+        form = CalendarEventForm(request.POST, instance=event, user=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, _('事件已成功更新！'))
+            return redirect('dep_calendar:event_list')
+    else:
+        form = CalendarEventForm(instance=event, user=request.user)
+    
+    return render(request, 'dep_calendar/event_form.html', {
+        'form': form, 
+        'form_buttons': form_buttons,
+        'event': event
+    })
+
+@login_required
+def event_delete(request, pk):
+    event = get_object_or_404(CalendarEvent, id=pk)
+    
+    # 檢查權限 - 只有同部門的人才能刪除
+    if not event.can_user_access(request.user):
+        return HttpResponseForbidden("您沒有權限刪除此事件")
+    
+    if request.method == 'POST':
+        event_title = event.title
+        event.delete()
+        messages.success(request, _('事件「{}」已成功刪除！').format(event_title))
+        return redirect('dep_calendar:event_list')
+    
+    return render(request, 'dep_calendar/event_confirm_delete.html', {'event': event})
+
+@login_required
+def event_detail(request, pk):
+    event = get_object_or_404(CalendarEvent, id=pk)
+    
+    # 檢查權限 - 只有同部門的人才能查看
+    if not event.can_user_access(request.user):
+        return HttpResponseForbidden("您沒有權限查看此事件")
+    
+    return render(request, 'dep_calendar/event_detail.html', {'event': event})
