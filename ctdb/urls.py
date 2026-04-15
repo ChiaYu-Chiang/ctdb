499417d4a3134d47902cf7057894f4ed1af49c79
Brian Chiang <brian_chiang@chief.com.tw>
2026-04-15 10:11:49 +0800

    urls bug fix

--- ctdb/urls.py
+++ ctdb/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('', include('diary.urls')),
     path('', include('dep_calendar.urls')),
-    path('', include('ext_calendar.urls')),
     path('', include('reminder.urls')),
     path('', include('telecom.urls')),
     path('', include('archive.urls')),
